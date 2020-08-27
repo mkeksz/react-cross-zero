9b4115a0add25b8e7fdad93e51c7524d76df6ff8
mrkeksz <mrkeksz@yandex.ru>
2020-08-27 22:52:56 +0300

    Complete game

---
+++ docs/precache-manifest.1b175fcd42d454ed16c321d670c27234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a097bf805f693438b791aab1d6033e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7fc33f3402c5e94fbb4c",
+    "url": "/static/css/main.c7ab3b04.chunk.css"
+  },
+  {
+    "revision": "4266a9ce2cec289f87ae",
+    "url": "/static/js/2.47c5f4de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.47c5f4de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fc33f3402c5e94fbb4c",
+    "url": "/static/js/main.ab2f2475.chunk.js"
+  },
+  {
+    "revision": "d76d90f91fdbbe48edb7",
+    "url": "/static/js/runtime-main.0e755f5a.js"
+  }
+]);
